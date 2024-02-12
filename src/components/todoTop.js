70b3649d42e6fc9aaacc9e6f22b9e85dc4386387
ChaMin <kusoft1997@gmail.com>
2024-02-13 00:54:31 +0900

    feature) 다크모드 구현

--- src/components/todoTop.js
+++ src/components/todoTop.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 const TodoTopStyle=styled.div`
@@ -18,21 +18,37 @@ const TodoTopStyle=styled.div`
     .todoTopDark{
         width:40px;
         height:40px;
+        color:white;
     }
 `;
 
 function TodoTop(){
-    return(
-        <TodoTopStyle>
-            <div className="todoTopContainer">
-                <div className="todoTopTitle">
-                    RoadMap
-                </div>
-                <img className="todoTopDark" src="res/iconDark.svg"></img>
-                
-            </div>
-        </TodoTopStyle>
-    )
+    const [darkMode, setDarkMode] = useState(false);
+
+  const handleDarkMode = () => {
+    setDarkMode(!darkMode);
+    if (!darkMode) {
+      document.body.style.backgroundColor = "#282c34";
+      document.body.style.color = "#e9ecef";
+    } else {
+      document.body.style.backgroundColor = "#e9ecef";
+      document.body.style.color = "black";
+    }
+  };
+
+  return (
+    <TodoTopStyle>
+      <div className={`todoTopContainer ${darkMode ? 'darkMode' : ''}`}>
+        <div className="todoTopTitle">RoadMap</div>
+        <img
+          className={`todoTopDark ${darkMode ? 'darkMode' : ''}`}
+          src={darkMode ? "res/iconLight.svg" : "res/iconDark.svg"}
+          alt="Dark mode"
+          onClick={handleDarkMode}
+        ></img>
+      </div>
+    </TodoTopStyle>
+  );
 }
 
 export default TodoTop;
