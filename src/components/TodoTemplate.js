08fb80fb7296d6e4c568117e37d8ef2c2dc0bb24
mung96 <mung5079@gmail.com>
2024-02-12 12:54:27 +0900

    feat: 템플릿역할(TodoTemplate.js) ui 구현

--- src/components/TodoTemplate.js
+++ src/components/TodoTemplate.js
@@ -1,5 +1,11 @@
+import "../scss/TodoTemplate.scss";
+
 const TodoTemplate = ({ children }) => {
-  return <div>{children}</div>;
+  return (
+    <div className="TodoTemplate">
+      <h1 className="title">RoadMap</h1>
+    </div>
+  );
 };
 
 export default TodoTemplate;
