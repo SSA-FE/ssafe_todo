48eb71a00f5c04a5512ab6a63a523210d35d4053
mung96 <mung5079@gmail.com>
2024-02-14 15:36:47 +0900

    bug

--- src/components/TodoTemplate.js
+++ src/components/TodoTemplate.js
@@ -11,14 +11,17 @@ const TodoTemplate = ({ children }) => {
   const [completes, setCompletes] = useState(
     jsonLocalStorage.getItem("completes") || []
   );
+
   const [movedItem, setMovedItem] = useState(null);
+
   const handleMoveTodoBtn = (id) => {
-    //얘가 여기 있어도 되나? 이걸로 기능 구현은 될 것 같은데, 좀 어색하네. 여기서부터 workItem으로 내려가는게 좀 불합리해보인다.
     setCompletes(jsonLocalStorage.getItem("completes"));
     console.log(completes);
+
     setMovedItem(completes.find((item) => item.id === id));
-    const nextItems = [...todos, movedItem];
     console.log(movedItem);
+
+    const nextItems = [...todos, movedItem];
     setTodos(nextItems);
     jsonLocalStorage.setItem("todos", nextItems);
   };
